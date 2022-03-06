78806b9431af300131d25e7fc2a4111845f1c6d8
Vanshika Mittal <97899754+VanshikaM03@users.noreply.github.com>
2022-03-06 13:27:28 +0530

    Add files via upload

---
+++ quiz (1).js
@@ -0,0 +1,24 @@
+function check() {
+    var c = 0;
+    var q1 = document.quiz.question1.value;
+    var q2 = document.quiz.question2.value;
+    var q3 = document.quiz.question3.value;
+    var q4 = document.quiz.question4.value;
+    var q5 = document.quiz.question5.value;
+    var q6 = document.quiz.question6.value;
+    var q7 = document.quiz.question7.value;
+    var q8 = document.quiz.question8.value;
+    var q9 = document.quiz.question9.value;
+    var q10 = document.quiz.question10.value;
+    if (q1 == "Netherlands") { c++ }
+    if (q2 == "Pink, indigo,turquoise") { c++ }
+    if (q3 == "Vermont") { c++ }
+    if (q4 == "Brazil") { c++ }
+    if (q5 == "Black, gray, white, purple") { c++ }
+    if (q6 == "How you experience your gender internally") { c++ }
+    if (q7 == "All of these") { c++ }
+    if (q8 == "25%") { c++ }
+    if (q9 == "Businesses are in many ways leading the charge in the fight against LGBT discrimination.") { c++ }
+    if (q10 == "Update antidiscrimination laws to include LGBT people.") { c++ }
+    document.write("You scored ", c,);
+}
